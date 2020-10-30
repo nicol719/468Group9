f6c42f810fe116098075375aded930ff925425fb
GRANT-PC\gnico_000 <g.nicol93@gmail.com>
2020-10-29 19:26:59 -0700

    Added Lecture Code
    Added code from Testbench - Memory.pdf in the lecture files

--- RAM.v
+++ RAM.v
@@ -1,1 +1,17 @@
 //RAM section here
+//Read and write operations of memory. 
+// Memory size is 8 words of 16 bits each. 
+module memory (Enable,ReadWrite,Address,DataIn,DataOut);
+input  Enable,ReadWrite;
+input [15:0] DataIn;
+input [2:0] Address;
+output reg[15:0] DataOut;
+reg [15:0] Mem [0:7];         //8 x 16 memory
+always @*
+	if (Enable)
+		if (ReadWrite) 
+			DataOut = Mem[Address];  //Read
+		else
+			Mem[Address] = DataIn;   //Write
+	else DataOut = 16'bz;   //High imped 
+endmodule
