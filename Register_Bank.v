a7b23d4224dcdef39b51ce23ca6e77c3c83742a5
GRANT-PC\gnico_000 <g.nicol93@gmail.com>
2020-11-04 14:07:57 -0800

    Added register bank requirements

--- Register_Bank.v
+++ Register_Bank.v
@@ -1,1 +1,18 @@
 // Register Bank Here
+// Register Bank consists of a 4 to 16 decoder, 16 32-bit registers, and 2 32-bit 16 x 1 mux's
+
+module register_bank;
+	//register bank code here
+endmodule
+
+module decoder_4to16;
+	//decoder code goes here
+endmodule
+
+module register_32bit;
+	//register code goes here
+endmodule
+
+module mux_16x1;
+	//mux code goes here
+endmodule
