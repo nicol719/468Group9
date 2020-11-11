59e795a698c35be3e94eca74af96859f9c4c87a9
Ji-Zhan <74079044+Ji-Zhan@users.noreply.github.com>
2020-11-11 15:43:16 -0800

    Add 32 bits ADDER

--- ALU.v
+++ ALU.v
@@ -188,7 +188,7 @@ module ALU (OP_Code, source_1, source_2, shift_bits, conditional, S, Result, fla
   else if (conditional == 4'b0101)
     begin
       //Less than or equal to
-      source_1 <= source_2
+      source_1 =< source_2
     end
   else if (conditional == 4'b0110)
     begin
@@ -240,6 +240,40 @@ module ALU (OP_Code, source_1, source_2, shift_bits, conditional, S, Result, fla
 // 32-bit adder
 //=================
 //Code here
+module N_bit_adder(input1,input2,answer);
+parameter N=32;
+input [N-1:0] input1,input2;
+   output [N-1:0] answer;
+   wire  carry_out;
+  wire [N-1:0] carry;
+   genvar i;
+   generate 
+   for(i=0;i<N;i=i+1)
+     begin: generate_N_bit_Adder
+   if(i==0) 
+  half_adder f(input1[0],input2[0],answer[0],carry[0]);
+   else
+  full_adder f(input1[i],input2[i],carry[i-1],answer[i],carry[i]);
+     end
+  assign carry_out = carry[N-1];
+   endgenerate
+endmodule 
+
+// Verilog code for half adder 
+module half_adder(x,y,s,c);
+   input x,y;
+   output s,c;
+   assign s=x^y;
+   assign c=x&y;
+endmodule // half adder
+
+// Verilog code for full adder 
+module full_adder(x,y,c_in,s,c_out);
+   input x,y,c_in;
+   output s,c_out;
+ assign s = (x^y) ^ c_in;
+ assign c_out = (y&c_in)| (x&y) | (x&c_in);
+endmodule // full_adder
 
 //==================
 // 32-bit subtractor
