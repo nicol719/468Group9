52cef52c2f1f9d5384797c74eba9e28708e4ac5e
Ji-Zhan <74079044+Ji-Zhan@users.noreply.github.com>
2020-11-11 16:20:14 -0800

    32-bit ADDER

--- ALU_testbench.v
+++ ALU_testbench.v
@@ -31,6 +31,28 @@
 //=================
 //Code here
 
+module tb_32_bit_adder;
+ // Inputs
+ reg [31:0] input1;
+ reg [31:0] input2;
+ // Outputs
+ wire [31:0] answer;
+	
+32_bit_adder uut (
+  .input1(input1), 
+  .input2(input2), 
+  .answer(answer)
+ );
+
+initial begin
+  // Initialize Inputs
+  input1 = 1209;
+  input2 = 4565;
+  #100;
+  // Add stimulus here
+ end
+      
+endmodule
 //==================
 // 32-bit subtractor
 //=================
