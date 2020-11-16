51609f8096b68e8987ee6af034bdb3f8afd1799d
GRANT-PC\gnico_000 <g.nicol93@gmail.com>
2020-11-15 16:32:31 -0800

    Updated RAM testbenches to 32 Bit

--- RAM_read_testbench.v
+++ RAM_read_testbench.v
@@ -1,25 +1,25 @@
 
 //Todo make testbench 32 bits
-
+// Read Testbench by GN
 module test_read_memory;
     reg  enable,R_W;
-    reg [15:0] D_In;
-    reg [2:0] Address;
-    wire [15:0] D_Out;
+    reg [31:0] D_In;
+    reg [15:0] Address;
+    wire [31:0] D_Out;
 
 memory ram(enable,R_W,Address,D_In,D_Out); 
 
 initial  
 begin
-   enable =0;   R_W=0;	Address=3'd0;	D_In =16'hAAAA;
-#5 enable =1;   R_W=0;	Address=3'd0;	D_In =16'hAAAA;
-#5 enable =1;   R_W=0;	Address=3'd1;	D_In =16'h00AA;
-#5 enable =1;   R_W=0;	Address=3'd2;	D_In =16'h00BB;
-#5 enable =1;   R_W=0;	Address=3'd3;	D_In =16'h00CC;
-#5 enable =1;   R_W=0;	Address=3'd4;	D_In =16'h00DD;
-#5 enable =1;   R_W=0;	Address=3'd5;	D_In =16'h00EE;
-#5 enable =1;   R_W=0;	Address=3'd6;	D_In =16'h00FF;
-#5 enable =1;   R_W=0;	Address=3'd7;	D_In =16'hFFFF;
+   enable =0;   R_W=0;	Address=16'd0;	D_In =32'hAAAA0000;
+#5 enable =1;   R_W=0;	Address=16'd0;	D_In =32'hAAAA0000;
+#5 enable =1;   R_W=0;	Address=16'd1;	D_In =32'h00AA0000;
+#5 enable =1;   R_W=0;	Address=16'd2;	D_In =32'h00BB0000;
+#5 enable =1;   R_W=0;	Address=16'd3;	D_In =32'h00CC0000;
+#5 enable =1;   R_W=0;	Address=16'd4;	D_In =32'h00DD0000;
+#5 enable =1;   R_W=0;	Address=16'd5;	D_In =32'h00EE0000;
+#5 enable =1;   R_W=0;	Address=16'd6;	D_In =32'h00FF0000;
+#5 enable =1;   R_W=0;	Address=16'd7;	D_In =32'hFFFF0000;
 #10
 
   $writememh("data_h.txt", ram.Mem);
--- RAM_write_testbench.v
+++ RAM_write_testbench.v
@@ -1,9 +1,10 @@
-//Todo: Convert testbench to 32 bit
+//Write Testbench
+// By GN
 module test_write_memory;
     reg  enable,ReadWrite;
-    reg [15:0] DataIn;
-    reg [2:0] Address;
-    wire [15:0] DataOut;
+    reg [31:0] DataIn;
+    reg [15:0] Address;
+    wire [31:0] DataOut;
 
 memory ram(enable,ReadWrite,Address,DataIn,DataOut); 
 
@@ -12,15 +13,15 @@ begin
 
   $readmemh("data_h.txt", ram.Mem);
 
-   enable =0;   ReadWrite=0;	Address=3'd0;	
-#5 enable =1;   ReadWrite=1;	Address=3'd0;	
-#5 enable =1;   ReadWrite=1;	Address=3'd1;	
-#5 enable =1;   ReadWrite=1;	Address=3'd2;	
-#5 enable =1;   ReadWrite=1;	Address=3'd3;	
-#5 enable =1;   ReadWrite=1;	Address=3'd4;	
-#5 enable =1;   ReadWrite=1;	Address=3'd5;	
-#5 enable =1;   ReadWrite=1;	Address=3'd6;	
-#5 enable =1;   ReadWrite=1;	Address=3'd7;	
+   enable =0;   ReadWrite=0;	Address=16'd0;	
+#5 enable =1;   ReadWrite=1;	Address=16'd0;	
+#5 enable =1;   ReadWrite=1;	Address=16'd1;	
+#5 enable =1;   ReadWrite=1;	Address=16'd2;	
+#5 enable =1;   ReadWrite=1;	Address=16'd3;	
+#5 enable =1;   ReadWrite=1;	Address=16'd4;	
+#5 enable =1;   ReadWrite=1;	Address=16'd5;	
+#5 enable =1;   ReadWrite=1;	Address=16'd6;	
+#5 enable =1;   ReadWrite=1;	Address=16'd7;	
 
 end
 
